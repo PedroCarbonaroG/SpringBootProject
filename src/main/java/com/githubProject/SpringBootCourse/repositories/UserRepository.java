a545e552612bce0d7482d6daf97cbbe81ae44455
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-03-31 17:36:46 -0300

    feature: component registration, findById implementated

--- src/main/java/com/githubProject/SpringBootCourse/repositories/UserRepository.java
+++ src/main/java/com/githubProject/SpringBootCourse/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package com.githubProject.SpringBootCourse.repositories;
 
 import com.githubProject.SpringBootCourse.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
 }

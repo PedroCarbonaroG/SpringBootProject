054f95d3478158aa1e55ec773536093fd9bd3042
Pedro Carbonaro <pedrocarbonaro73@gmail.com>
2024-03-31 16:29:21 -0300

    fix: UserResourceClass

--- src/main/java/com/githubProject/SpringBootCourse/SpringBootProject.java
+++ src/main/java/com/githubProject/SpringBootCourse/SpringBootProject.java
@@ -1,4 +1,4 @@
-package com.githubProject.SpringBootCourse.app;
+package com.githubProject.SpringBootCourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/githubProject/SpringBootCourse/resources/UserResource.java
+++ src/main/java/com/githubProject/SpringBootCourse/resources/UserResource.java
@@ -1,19 +1,19 @@
 package com.githubProject.SpringBootCourse.resources;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.githubProject.SpringBootCourse.entities.User;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/t")
+@RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Pedro", "pedro@gmail.com", "3199999", "12345");
+        User u = new User(1L, "Pedro", "pedro@gmail.com", "99999999", "12345");
         return ResponseEntity.ok().body(u);
     }
 }

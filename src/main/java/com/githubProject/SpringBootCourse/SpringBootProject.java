ff9bdb96e8c12f7b146cfb3e2a86d8fc2977db19
PedroCarbonaroG <pedrocarbonaro73@gmail.com>
2024-04-01 15:58:26 -0300

    feature: OrderStatus enum

--- src/main/java/com/githubProject/SpringBootCourse/SpringBootProject.java
+++ src/main/java/com/githubProject/SpringBootCourse/SpringBootProject.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootProject {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProject.class, args);
 	}
-
 }
--- src/main/java/com/githubProject/SpringBootCourse/entities/Order.java
+++ src/main/java/com/githubProject/SpringBootCourse/entities/Order.java
@@ -1,6 +1,7 @@
 package com.githubProject.SpringBootCourse.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.githubProject.SpringBootCourse.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,16 +19,25 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {
     }
-    public Order(Integer id, Instant moment, User client) {
+    public Order(Integer id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+    public Order(Order order) {
+        this.id = order.getId();
+        this.moment = order.getMoment();
+        this.setOrderStatus(order.getOrderStatus());
+        this.client = order.getClient();
     }
 
     public Integer getId() {
@@ -38,11 +48,11 @@ public class Order implements Serializable {
         this.id = id;
     }
 
-    public Instant getDate() {
+    public Instant getMoment() {
         return moment;
     }
 
-    public void setDate(Instant moment) {
+    public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
@@ -52,6 +62,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
